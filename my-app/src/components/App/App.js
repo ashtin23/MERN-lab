2584b90e7cd93e0bb2f6047875ab6f823f0708f7
ashtinlatting <rlatting92@gwu.edu>
2019-08-15 15:54:36 -0400

    connected to backend

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,55 +1,36 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Switch
-} from 'react-router-dom';
+import {Router,Link,Route} from 'react-router-dom';
 import Movie from '../Movie/Movie';
-import './App.css';
-import axios from 'axios';
+// import './App.css';
+
 
 // const url = 'http://localhost:3001/api/movies'
 
 class App extends Component  {
-  constructor() {
-    super();
-    this.state = {
-      movies: null
-    };
-    // this.getMovies = this.getMovies.bind(this);
-  }
 
-
-  // delete(){}
-
-  componentDidMount() {
-    axios.get("http://localhost:3001/api/movies")
-    .then(res => {
-      console.log(res)
-      this.setState({ movies: res.data })
-    })
-  }
+  // componentDidMount() {
+  //   axios.get("http://localhost:3001/api/movies")
+  //   .then(res => {
+  //     console.log(res)
+  //     this.setState({ movies: res.data })
+  //   })
+  // }
 
   render() {
   return (
-    <Router>
+    
     <div className="App">
-      <nav className="App-header">
+
+      <nav>
         <Link to="/movies">Movies</Link>
       </nav>
-      <Switch>
-        <Route
-          path="/"
-          render={props => <Movie {...props}{...this.state} />}
-          />
-        <Route
-          path="/movie"
-          render={props => <Movie {...props}{...this.state} />}
-          />
-      </Switch>
+
+      <main>
+        <Route path="/" component={Movie} />
+      </main>  
+
     </div>
-    </Router>
+    
   )
   };
 }
--- my-app/src/components/Movie/Movie.js
+++ my-app/src/components/Movie/Movie.js
@@ -1,55 +1,40 @@
 import React, { Component } from "react";
 import axios from 'axios';
-import {
-    // BrowserRouter as Router,
-    Link,
-    Route,
-  } from 'react-router-dom'
-// import { Link } from "react-router-dom";
+
 
 class Movie extends Component {
-	constructor(props) {
-		super(props);
+	constructor(){
+		super()
 		this.state = {
-			movies: []
-		}
+			movies: [],
+		 }
     }
 
     componentDidMount() {
         axios.get('http://localhost:3001/api/movies')
         .then((res) => {
             console.log(res)
-            this.setState({
-                movies: res.data
-            })
+            this.setState({movies: res.data})
         })
-        .catch((err) => {
-            console.log(err)
-        })
+        // .catch((err) => {
+        //     console.log(err)
+        // })
     }
     
     render () {
-        const movies = this.state.movies.map((movie, index) => {
-          return (
-            <div key={ index }>
+         return (
+            <div className="movie">
               <p>
-                <Link to={`/movies/${movie._id}`}>{ movie.movie }</Link>
+             {this.state.movies.map(movie => <li key={movie.movie}>{movie.movie}</li>)}
               </p>
-              <Route
-                path={`/movies/${movie._id}`}
-              />
+              
             </div>
           )
-        })
+        }
     
-        return (
-          <div>
-            <h1>Movies</h1>
-            { movies }
-          </div>
-        )
+       
       
-    }
+      
 }
 
 
